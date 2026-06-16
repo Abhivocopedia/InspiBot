7a57aae6e40ce1faa8685d8747940957d80c391d
Abhinandana Bhatta <abhivocopedia@gmail.com>
2026-06-16 15:33:48 +0530

    Replace ESP32 camera with OV7670 driver

--- esp.c
+++ esp.c
@@ -1,31 +1,13 @@
 #include <WiFi.h>
-#include "esp_camera.h"
 #include <WebServer.h>
+#include "OV7670.h"   // use an OV7670 driver library
 
 // ===== WiFi AP =====
 const char* ssid = "PipeBot";
 const char* password = "12345678";
 
-// ===== Camera Pins (AI Thinker ESP32-CAM) =====
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM     0
-#define SIOD_GPIO_NUM    26
-#define SIOC_GPIO_NUM    27
-
-#define Y9_GPIO_NUM      35
-#define Y8_GPIO_NUM      34
-#define Y7_GPIO_NUM      39
-#define Y6_GPIO_NUM      36
-#define Y5_GPIO_NUM      21
-#define Y4_GPIO_NUM      19
-#define Y3_GPIO_NUM      18
-#define Y2_GPIO_NUM       5
-#define VSYNC_GPIO_NUM   25
-#define HREF_GPIO_NUM    23
-#define PCLK_GPIO_NUM    22
-
 WebServer server(80);
+OV7670 camera;
 
 // ===== MJPEG stream handler =====
 void handleStream() {
@@ -35,12 +17,14 @@ void handleStream() {
   client.print(response);
 
   while (client.connected()) {
-    camera_fb_t * fb = esp_camera_fb_get();
-    if (!fb) continue;
-    client.printf("--frame\r\nContent-Type: image/jpeg\r\nContent-Length: %d\r\n\r\n", fb->len);
-    client.write(fb->buf, fb->len);
+    // Grab a frame from OV7670
+    camera.captureFrame();
+    uint8_t* buf = camera.getFrameBuffer();
+    size_t len = camera.getFrameSize();
+
+    client.printf("--frame\r\nContent-Type: image/jpeg\r\nContent-Length: %d\r\n\r\n", len);
+    client.write(buf, len);
     client.print("\r\n");
-    esp_camera_fb_return(fb);
     delay(50);
   }
 }
@@ -48,36 +32,12 @@ void handleStream() {
 void setup() {
   Serial.begin(115200);
 
-  // Camera config
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-  config.frame_size = FRAMESIZE_QVGA;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-
-  if (esp_camera_init(&config) != ESP_OK) {
-    Serial.println("Camera init failed");
+  // Initialize camera
+  if (!camera.begin(QQVGA, PIXFORMAT_JPEG)) {
+    Serial.println("Camera init failed!");
     return;
   }
+  Serial.println("Camera initialized.");
 
   // Start WiFi AP
   WiFi.softAP(ssid, password);
@@ -86,7 +46,6 @@ void setup() {
 
   // Routes
   server.on("/stream", [](){ handleStream(); });
-
   server.begin();
   Serial.println("Server started");
 }
